6d09c44a1a2188f3e128c88847bbabaf9b0c27b1
Johannes Brodwall <johannes@brodwall.com>
2023-09-26 23:52:49 +0200

    Example of spec with readOnly and writeOnly fields

---
+++ snapshotTests/snapshot/readOnlyWriteOnly/test/modelTest.ts
@@ -0,0 +1,354 @@
+import {
+    UserDto,
+    UserPermissionDto,
+    UserPermissionDtoPermissionEnumValues,
+} from "../model";
+
+export class Random {
+    seed: number;
+    constructor(seed: number | string) {
+        this.seed = this.hash(seed) % 2147483647;
+        if (this.seed <= 0) this.seed += 2147483646;
+    }
+
+    next(): number {
+        this.seed = (this.seed * 16807) % 2147483647;
+        return this.seed;
+    }
+
+    nextFloat(): number {
+        return (this.next() - 1) / 2147483646;
+    }
+
+    nextInt(limit: number): number {
+        return this.next() % limit;
+    }
+
+    nextnumber(limit: number): number {
+        return this.next() % limit;
+    }
+
+    nextBoolean(): boolean {
+        return this.nextInt(2) == 0;
+    }
+
+    pickOne<T>(options: readonly T[]): T {
+        return options[this.nextInt(options.length)];
+    }
+
+    pickSome<T>(options: readonly T[], n?: number): T[] {
+        const shuffled = [...options].sort(() => 0.5 - this.next());
+        return shuffled.slice(0, n || this.nextInt(options.length));
+    }
+
+    uuidv4(): string {
+        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+            const r = this.nextInt(16) | 0;
+            const v = c == "x" ? r : (r & 0x3) | 0x8;
+            return v.toString(16);
+        });
+    }
+
+    hash(seed: string | number): number {
+        if (typeof seed === "number") {
+            return seed < 0 ? Math.floor(seed*1000) : Math.floor(seed);
+        }
+        return seed.split("").reduce((a, b) => {
+            a = (a << 5) - a + b.charCodeAt(0);
+            return a & a;
+        }, 0);
+    }
+}
+
+export type Factory<T> = {
+    [P in keyof T]?: ((sampleData: TestSampleData) => T[P]) | T[P];
+};
+
+type ModelFactory<T> = Factory<T> | ((testData: TestSampleData) => T);
+
+export interface SampleModelFactories {
+    UserDto?: ModelFactory<UserDto>;
+    UserPermissionDto?: ModelFactory<UserPermissionDto>;
+}
+
+export interface SamplePropertyValues {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [key: string]: (sampleData: TestSampleData) => any;
+}
+
+export interface TestData {
+    seed?: number | string;
+    sampleModelProperties?: SampleModelFactories;
+    samplePropertyValues?: SamplePropertyValues;
+    now?: Date;
+}
+
+export interface PropertyDefinition {
+    containerClass: string;
+    propertyName: string;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    example?: string | null | Array<any>;
+    isNullable?: boolean;
+}
+
+export class TestSampleData {
+    random: Random;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    sampleModelProperties: any;
+    samplePropertyValues: SamplePropertyValues;
+    now: Date;
+
+    constructor({ seed, sampleModelProperties, samplePropertyValues, now }: TestData) {
+        this.random = new Random(seed || 100);
+        this.now = now || new Date(2019, 1, this.random.nextInt(2000));
+        this.sampleModelProperties = sampleModelProperties || {};
+        this.samplePropertyValues = samplePropertyValues || {};
+    }
+
+    nextFloat(): number {
+        return this.random.nextFloat();
+    }
+
+    nextInt(limit: number): number {
+        return this.random.nextInt(limit);
+    }
+
+    nextBoolean(): boolean {
+        return this.random.nextBoolean();
+    }
+
+    sampleboolean(): boolean {
+        return this.random.nextBoolean();
+    }
+
+    pickOne<T>(options: readonly T[]): T {
+        return this.random.pickOne(options);
+    }
+
+    pickOneString<T extends string>(options: readonly T[]): T {
+        return this.random.pickOne(options);
+    }
+
+    pickSome<T>(options: readonly T[]): T[] {
+        return this.random.pickSome(options);
+    }
+
+    uuidv4(): string {
+        return this.random.uuidv4();
+    }
+
+    randomString(): string {
+        return this.pickOne(["foo", "bar", "baz"]);
+    }
+
+    randomArray<T>(generator: (n: number) => T, length?: number): readonly T[] {
+        if (!length) length = this.nextInt(3) + 1;
+        return Array.from({ length }).map((_, index) => generator(index));
+    }
+
+    randomEmail(): string {
+        return (
+            this.randomFirstName().toLowerCase() +
+            "." +
+            this.randomLastName().toLowerCase() +
+            "@" +
+            this.randomDomain()
+        );
+    }
+
+    randomFirstName(): string {
+        return this.pickOne(["James", "Mary", "John", "Patricia", "Robert", "Jennifer", "Linda"]);
+    }
+
+    randomLastName(): string {
+        return this.pickOne(["Smith", "Williams", "Johnson", "Jones", "Brown", "Davis", "Wilson"]);
+    }
+
+    randomFullName(): string {
+        return this.randomFirstName() + " " + this.randomLastName();
+    }
+
+    randomDomain(): string {
+        return (
+            this.pickOne(["a", "b", "c", "d", "e"]) +
+            ".example." +
+            this.pickOne(["net", "com", "org"])
+        );
+    }
+
+    randomPastDateTime(now: Date): Date {
+        return new Date(now.getTime() - this.nextInt(4 * 7 * 24 * 60 * 60 * 1000));
+    }
+
+    sampleDateTime(): Date {
+        return this.randomPastDateTime(this.now);
+    }
+
+    samplenumber(): number {
+        return this.nextInt(10000);
+    }
+
+    sampleunknown(): unknown {
+        return {
+            [this.randomString()]: this.randomString(),
+        }
+    }
+
+    sampleDate(): Date {
+        return this.randomPastDateTime(this.now);
+    }
+
+    sampleString(dataFormat?: string, example?: string): string {
+        if (dataFormat === "uuid") {
+            return this.uuidv4();
+        }
+        if (dataFormat === "uri") {
+            return "https://" + this.randomDomain() + "/" + this.randomFirstName().toLowerCase();
+        }
+        if (dataFormat === "email") {
+            return this.randomEmail();
+        }
+        if (example && example !== "null") return example;
+        return this.randomString();
+    }
+
+    sampleArrayString(length?: number): Array<string> {
+        return Array.from({ length: length || this.arrayLength() }).map(() => this.sampleString());
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    sampleArrayArray<T>(length?: number): readonly T[] {
+        return [];
+    }
+
+    sampleArraynumber(length?: number): Array<number> {
+        return Array.from({ length: length || this.arrayLength() }).map(() => this.samplenumber());
+    }
+
+    generate(
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        template?: ((sampleData: TestSampleData) => any) | any,
+        propertyDefinition?: PropertyDefinition,
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        generator?: () => any
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ): any {
+        if (template != undefined) {
+            return typeof template === "function" ? template(this) : template;
+        }
+        if (propertyDefinition) {
+            const { containerClass, propertyName, example } = propertyDefinition;
+            if (this.sampleModelProperties[containerClass]) {
+                const propertyFactory = this.sampleModelProperties[containerClass][propertyName];
+                if (propertyFactory && typeof propertyFactory === "function") {
+                    return propertyFactory(this);
+                } else if (propertyFactory !== undefined) {
+                    return propertyFactory;
+                }
+            }
+            if (this.samplePropertyValues[propertyName] !== undefined) {
+                return this.samplePropertyValues[propertyName](this);
+            }
+            if (example && example !== "null") return example;
+        }
+        return generator && generator();
+    }
+
+    arrayLength(): number {
+        return this.nextInt(3) + 1;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    sample(modelName: string): any {
+        switch (modelName) {
+            case "UserDto":
+                return this.sampleUserDto();
+            case "Array<UserDto>":
+                return this.sampleArrayUserDto();
+            case "UserPermissionDto":
+                return this.sampleUserPermissionDto();
+            case "Array<UserPermissionDto>":
+                return this.sampleArrayUserPermissionDto();
+            default:
+                throw new Error("Unknown type " + modelName);
+        }
+    }
+
+    sampleUserDto(template?: Factory<UserDto>): UserDto {
+        const containerClass = "UserDto";
+        if (!template && typeof this.sampleModelProperties[containerClass] === "function") {
+            return this.sampleModelProperties[containerClass](this);
+        }
+        return {
+            id: this.generate(
+                template?.id,
+                { containerClass, propertyName: "id", example: "null", isNullable: false },
+                () => this.samplenumber()
+            ),
+            username: this.generate(
+                template?.username,
+                { containerClass, propertyName: "username", isNullable: false },
+                () => this.sampleString("", "null")
+            ),
+            emailAddress: this.generate(
+                template?.emailAddress,
+                { containerClass, propertyName: "emailAddress", isNullable: false },
+                () => this.sampleString("email", "null")
+            ),
+            updatedAt: this.generate(
+                template?.updatedAt,
+                { containerClass, propertyName: "updatedAt", example: "null", isNullable: false },
+                () => this.sampleDate()
+            ),
+            password: this.generate(
+                template?.password,
+                { containerClass, propertyName: "password", isNullable: false },
+                () => this.sampleString("", "null")
+            ),
+            permissions: this.generate(
+                template?.permissions,
+                { containerClass, propertyName: "permissions", example: null, isNullable: false },
+                () => this.sampleArrayUserPermissionDto()
+            ),
+        };
+    }
+
+    sampleArrayUserDto(
+        length?: number,
+        template?: Factory<UserDto>
+    ): readonly UserDto[] {
+        return this.randomArray(
+            () => this.sampleUserDto(template),
+            length ?? this.arrayLength()
+        );
+    }
+
+    sampleUserPermissionDto(template?: Factory<UserPermissionDto>): UserPermissionDto {
+        const containerClass = "UserPermissionDto";
+        if (!template && typeof this.sampleModelProperties[containerClass] === "function") {
+            return this.sampleModelProperties[containerClass](this);
+        }
+        return {
+            permission: this.generate(
+                template?.permission,
+                { containerClass, propertyName: "permission", example: "null", isNullable: false },
+                () => this.pickOne(UserPermissionDtoPermissionEnumValues)
+            ),
+            updatedAt: this.generate(
+                template?.updatedAt,
+                { containerClass, propertyName: "updatedAt", example: "null", isNullable: false },
+                () => this.sampleDate()
+            ),
+        };
+    }
+
+    sampleArrayUserPermissionDto(
+        length?: number,
+        template?: Factory<UserPermissionDto>
+    ): readonly UserPermissionDto[] {
+        return this.randomArray(
+            () => this.sampleUserPermissionDto(template),
+            length ?? this.arrayLength()
+        );
+    }
+}
